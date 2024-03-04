02777f049dbe144e0721dd8dba6b0cfa1e408b0a
SKRTEEEEEE <adanreh.m@gmail.com>
2024-03-04 09:18:58 +0100

    Formulario base
    Formulario con los mismos inputs que el ejemplo
    Css basico
    Faltaria copiar css ejemplo si es necesario

---
+++ 02_JS/03_FormularioHotel/script.js
@@ -0,0 +1,13 @@
+function eliminar() {
+    document.getElementById("miFormulario").reset();
+}
+
+function mostrarAlerta() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var adultos = document.getElementById("adultos").value;
+    var kids = document.getElementById("kids").value;
+    var opciones = document.querySelector('input[name="opciones"]:checked').value;
+
+    alert("Nombre: " + nombre + "\nApellido: " + apellido + "\nAdultos: " + adultos + "\nNiños " + kids + "Regimen comidas: " +opciones)
+  }
